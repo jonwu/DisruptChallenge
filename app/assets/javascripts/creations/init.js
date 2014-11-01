2afd2014cb8358a5803bd089c31ac557ae4f72ea
jonwu <jonwu92@gmail.com>
2014-10-31 20:33:39 -0700

    Style ask question to bottom

--- app/assets/javascripts/creations/init.js
+++ app/assets/javascripts/creations/init.js
@@ -1,7 +1,20 @@
-$(document).ready(function(){
-  $('#category_title_input').keydown(function(evnt){
+jQuery(document).ready(function($) {
+	// initializeForm()
+	titleListener()
+});
+
+function titleListener(){
+	$('#category_title_input').keydown(function(evnt){
     if(evnt.keyCode === 13){
       $('#category_title_form').submit();
     }
   });
-});
+}
+
+function initializeForm(){
+	$.fn.wysihtml5.defaultOptions['toolbar']['image'] = false;
+	console.log($.fn.wysihtml5.defaultOptions)
+	$('.question-form').wysihtml5();
+
+}
+
