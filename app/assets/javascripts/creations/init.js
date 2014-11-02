de5e8ce570973b3f8ba272907d35b6e89dc53a64
jonwu <jonwu92@gmail.com>
2014-11-01 20:17:24 -0700

    add documentation and clear textarea after submit

--- app/assets/javascripts/creations/init.js
+++ app/assets/javascripts/creations/init.js
@@ -1,6 +1,7 @@
 jQuery(document).ready(function($) {
 	// initializeForm()
 	titleListener()
+	addQuestionListener();
 });
 
 function titleListener(){
@@ -16,4 +17,25 @@ function initializeForm(){
 	console.log($.fn.wysihtml5.defaultOptions)
 	$('.question-form').wysihtml5();
 
+}
+
+function addQuestionListener(){
+	$(".add-question").click(function(event) {
+		$.ajax({
+			url: '/creations/check_question',
+			type: 'POST',
+			data: {"category": this.id},
+		})
+		.done(function() {
+			$(".question-form").val('')
+			console.log("success");
+		})
+		.fail(function() {
+			console.log("error");
+		})
+		.always(function() {
+			console.log("complete");
+		});
+		
+	});
 }
