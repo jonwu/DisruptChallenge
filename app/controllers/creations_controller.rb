8b90ed46003d779ecbaa01b9f10dfd70ffdec5b3
haoguan <haohowardguan@gmail.com>
2014-11-02 20:52:27 -0800

    added edge cases check to adding categories

--- app/controllers/creations_controller.rb
+++ app/controllers/creations_controller.rb
@@ -9,13 +9,12 @@ class CreationsController < ApplicationController
   end
 
   def add_new_category
-    count = Category.count + 1
-    @active_category = Category.create!( rfi_id: get_current_rfi.id,
-                  text: "default" + count.to_s)
     # get current Rfi
     @rfi = get_current_rfi
     @categories = @rfi.categories.all
-
+    text = find_available_text(@categories, "default")
+    @active_category = Category.create!( rfi_id: get_current_rfi.id,
+                  text: text)
   end 
 
   def update_active_category
@@ -27,11 +26,16 @@ class CreationsController < ApplicationController
   end
 
   def update_category_titles
-    @active_category = Category.find_by_id(params[:category_id])
-    @active_category.update_attributes(:text => params[:category_title])
     # get current Rfi
     @rfi = get_current_rfi
     @categories = @rfi.categories.all
+    @active_category = Category.find_by_id(params[:category_id])
+    text = params[:category_title]
+    # only update if the name actually changed
+    if @active_category.text.to_s != params[:category_title].to_s
+      text = find_available_text(@categories, text)
+      @active_category.update_attributes(:text => text)
+    end
   end
 
   def check_question
@@ -54,16 +58,26 @@ class CreationsController < ApplicationController
                                   impact: @impact)
     if @question.save
       redirect_to action: 'update_active_category', category: @category_id
+      return
     end
   end
 
   def delete_question
     Question.destroy(params[:question_id])
     redirect_to action: 'update_active_category', category: params[:category_id]
+    return
   end
 
   private
     def get_current_rfi
       return current_user.rfis.first
+    end
+
+    def find_available_text(categories, text)
+      count = 1
+      while categories.where(text: "_"*count + text).count > 0 do
+        count = count + 1
+      end
+      return "_"*count + text
     end
 end
