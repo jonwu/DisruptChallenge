9b3169ec6628c70b8c644d00eb938d1d793e7371
jonwu <jonwu92@gmail.com>
2014-11-25 12:19:07 -0800

    add sidebar css

--- app/assets/javascripts/dashboard/init.js
+++ app/assets/javascripts/dashboard/init.js
@@ -1,5 +1,13 @@
 jQuery(document).ready(function($) {
+    // var viewportHeight = $(window).height();
+    // alert(viewportHeight)
     checkBoxListener()
+    // setNewsFeedHeight(getContentHeight)
+});
+
+$(window).resize(function() {
+  // var viewportHeight = $(window).height();
+    // alert(viewportHeight)
 });
 
 function checkBoxListener() {
@@ -12,12 +20,34 @@ function checkBoxListener() {
     }
   });
 }
-
+function setNewsFeedHeight(h){
+  $(".news-feed").height(h)
+}
+function getContentHeight(){
+  return $(".container-fluid .row").height()
+}
 function update_selected(collaborator_id, is_selected){ 
   $.ajax({
     url: '/dashboard/update_selected',
     type: 'POST',
     data: {"collaborator_id": collaborator_id, "is_selected": is_selected},
+  })
+  .done(function() {
+    // console.log("success");
+  })
+  .fail(function() {
+    console.log("error");
+  })
+  .always(function() {
+    console.log("complete");
+  });
+  
+}
+
+function loadCharts(){
+  $.ajax({
+    url: '/dashboard/load_charts',
+    type: 'GET',
   })
   .done(function() {
     // console.log("success");
