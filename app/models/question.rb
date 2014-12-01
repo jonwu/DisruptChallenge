d1196e3fb3565365e53aa2da1c33e608de56764c
jonwu <jonwu92@gmail.com>
2014-12-01 02:38:28 -0800

    edit delete and made submissions dependent on questions

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,5 +2,6 @@ class Question < ActiveRecord::Base
   include PublicActivity::Common
 	belongs_to :category
 	has_many :responses, dependent: :destroy
+	has_many :submissions, dependent: :destroy
 
 end
