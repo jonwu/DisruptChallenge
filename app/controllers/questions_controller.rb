0fc75437b090df4d47793df79ae32e091a84f852
haoguan <haohowardguan@gmail.com>
2015-02-16 15:53:00 -0800

    add questions using rails forms

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -7,7 +7,6 @@ class QuestionsController < BaseController
 		if request.headers['X-PJAX']
 		    render :layout => false
 	  	end
-
 	end
 
 	def index
@@ -18,6 +17,19 @@ class QuestionsController < BaseController
 	def new
 		authenticate_category(params[:category_id])
 		initialize_template
+	end
+
+	def create
+		authenticate_category(params[:category_id])
+		p params
+		# TODO: Qualatative and Quantatative.
+    question = Question.create( category_id: params[:category_id],
+                                qual: true,
+                                quant: true,
+                                text: params[:question][:text],
+                                impact: params[:impact].strip)
+    # Create responses somehow for collaborators?
+    initialize_template
 	end
 
 end
