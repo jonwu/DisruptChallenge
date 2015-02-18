c270659aad66dd033e9b5e75f90b360fe537b8b8
jonwu <jonwu92@gmail.com>
2015-02-17 16:41:13 -0800

    fixed questions list css and added accordion

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,5 +76,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   config.assets.precompile += %w( responses.js )
+  config.assets.precompile += %w( questions.js )
   
 end
