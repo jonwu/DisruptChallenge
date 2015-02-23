e8af01500dba279c0451a827d07ce9f6d9f39735
jonwu <jonwu92@gmail.com>
2015-02-22 19:33:57 -0800

    add content partial to load children

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,9 +4,6 @@ class QuestionsController < BaseController
 	def show
 		authenticate_question(params[:id])
 		initialize_template
-		# if request.headers['X-PJAX']
-		#     render :layout => false
-	 	#  	end
 	end
 
 	def index
@@ -22,20 +19,23 @@ class QuestionsController < BaseController
 	def create
 		authenticate_category(params[:category_id])
 		# TODO: Qualatative and Quantatative.
-    question = Question.create( category_id: params[:category_id],
-                                qual: true,
-                                quant: true,
-                                text: params[:question][:text],
-                                impact: params[:impact].strip)
-    # Create responses somehow for collaborators?
-    initialize_template
+	    question = Question.create( category_id: params[:category_id],
+	                                qual: true,
+	                                quant: true,
+	                                text: params[:question][:text],
+	                                impact: params[:impact].strip)
+	    # Create responses somehow for collaborators?
+	    initialize_template
+	    
 	end
+
+	
 
 	def destroy 
 		p params
 		authenticate_question(params[:id])
 		question = Question.find_by_id(get_current_question.id)
-    Question.destroy(question.id)
+    	Question.destroy(question.id)
 		initialize_template
 	end
 
