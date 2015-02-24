20faa75e8542e2c7f459a1c179dfa600cb1c74a2
jonwu <jonwu92@gmail.com>
2015-02-24 15:23:08 -0800

    edit pjax

--- app/assets/javascripts/global/init.js
+++ app/assets/javascripts/global/init.js
@@ -6,6 +6,14 @@ $(document).ready(function() {
   /* Activating Best In Place */
   jQuery(".best_in_place").best_in_place();
   // $(document).pjax('[data-pjax] a, a[data-pjax]', '.pjax-container')
-  
+  toggleActive()
   
 });
+
+function toggleActive(){
+  $('#questions').on('click', ".list-group-item", function(event) {
+    $('#questions .active').removeClass('active');
+    $(this).addClass('active');
+  });
+}
+
